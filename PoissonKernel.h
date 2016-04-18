bac1ea1b8beab7373715b3532f923e7a79535844
Luca Frediani <luca.frediani@uit.no>
2016-04-18 14:43:08 +0200

    Added Green's kernel and Poisson's kernel

---
+++ PoissonKernel.h
@@ -0,0 +1,29 @@
+/*
+ *
+ *
+ *  \date Jul 7, 2009
+ *  \author Jonas Juselius <jonas.juselius@uit.no> \n
+ *          CTCC, University of Tromsø
+ *
+ * \breif
+ */
+
+#ifndef POISSONKERNEL_H_
+#define POISSONKERNEL_H_
+
+#include "GreensKernel.h"
+#include "macros.h"
+#include "TelePrompter.h"
+
+class PoissonKernel: public GreensKernel {
+public:
+    template<int D>
+        PoissonKernel(const MultiResolutionAnalysis<D> &mra): GreensKernel(mra) {
+        compKernRepr();
+    }
+    virtual ~PoissonKernel() {}
+protected:
+    virtual void compKernRepr();
+};
+
+#endif /* POISSONKERNEL_H_ */

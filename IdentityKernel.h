553308c072ecfb901d39cda7c261a1a3faf831e4
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-04-23 15:22:44 +0200

    Add IdentityKernel for construction of identity operator

---
+++ IdentityKernel.h
@@ -0,0 +1,18 @@
+#ifndef IDENTITYKERNEL_H
+#define IDENTITYKERNEL_H
+
+#include "GaussExp.h"
+
+class IdentityKernel : public GaussExp<1> {
+public:
+    IdentityKernel(double prec) {
+        double alpha = sqrt(1.0/prec);
+        double coef = pow(alpha/pi, 1.0/2.0);
+        double pos = 0.0;
+        GaussFunc<1> gauss(alpha, coef, &pos);
+        this->append(gauss);
+    }
+    virtual ~IdentityKernel() { }
+};
+
+#endif // IDENTITYKERNEL_H

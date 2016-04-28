602f84bf794ca3db03b1113c47a137cd9f785dc0
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-04-28 11:31:52 +0200

    Add operator(out, inp) to OperatorTree constructor

--- CrossCorrelationGenerator.h
+++ CrossCorrelationGenerator.h
@@ -5,6 +5,7 @@
 #include "WaveletAdaptor.h"
 #include "CrossCorrelationCalculator.h"
 #include "OperatorTree.h"
+#include "Timer.h"
 
 class CrossCorrelationGenerator : public TreeBuilder<2> {
 public:
@@ -14,16 +15,28 @@ public:
               build_prec(prec) { }
     virtual ~CrossCorrelationGenerator() { }
 
-    OperatorTree *operator()(FunctionTree<1> &kernel) {
-        this->adaptor = new WaveletAdaptor<2>(this->build_prec);
-        this->calculator = new CrossCorrelationCalculator(kernel, this->build_prec);
+    OperatorTree *operator()(FunctionTree<1> &inp) {
         OperatorTree *out = new OperatorTree(this->MRA);
-        this->build(*out);
-        out->mwTransform(BottomUp);
-        out->setupOperNodeCache();
+        (*this)(*out, inp);
+        return out;
+    }
+
+    void operator()(OperatorTree &out, FunctionTree<1> &inp) {
+        this->adaptor = new WaveletAdaptor<2>(this->build_prec);
+        this->calculator = new CrossCorrelationCalculator(inp, this->build_prec);
+        this->build(out);
         this->clearCalculator();
         this->clearAdaptor();
-        return out;
+
+        Timer trans_t;
+        trans_t.restart();
+        out.mwTransform(BottomUp);
+        out.calcSquareNorm();
+        out.setupOperNodeCache();
+        trans_t.stop();
+
+        println(10, "Time transform      " << trans_t);
+        println(10, std::endl);
     }
 protected:
     const double build_prec;

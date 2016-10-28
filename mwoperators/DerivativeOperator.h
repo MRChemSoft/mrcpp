60370a547ece8adfc935a7cb4174ffe8d1959ede
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-10-28 10:02:43 +0200

    remove gradient/divergence from DerivativeOperator

--- mwoperators/DerivativeOperator.h
+++ mwoperators/DerivativeOperator.h
@@ -2,22 +2,32 @@
 #define DERIVATIVEOPERATOR_H
 
 #include "MWOperator.h"
+#include "DerivativeGenerator.h"
 
 template<int D>
 class DerivativeOperator : public MWOperator {
 public:
     DerivativeOperator(const MultiResolutionAnalysis<D> &mra,
-                       double a = 0.5, double b = 0.5);
-    virtual ~DerivativeOperator();
-
-    void grad(FunctionTreeVector<D> &out, FunctionTree<D> &inp);
-    void div(FunctionTree<D> &out, FunctionTreeVector<D> &inp);
+                       double a = 0.5, double b = 0.5)
+            : MWOperator(mra.getOperatorMRA()),
+              A(a), B(b) {
+        if (this->A > MachineZero) NEEDS_TESTING;
+        if (this->B > MachineZero) NEEDS_TESTING;
+        initializeOperator();
+    }
+    virtual ~DerivativeOperator() { this->clearOperator(); }
 
 protected:
     const double A;
     const double B;
 
-    void initializeOperator();
+    void initializeOperator() {
+        DerivativeGenerator DG(this->oper_mra.getScalingBasis());
+
+        OperatorTree *oper_comp = new OperatorTree(this->oper_mra, MachineZero, MaxAllocOperNodes);
+        DG(*oper_comp, this->A, this->B);
+        this->oper_exp.push_back(oper_comp);
+    }
 };
 
 #endif // DERIVATIVEOPERATOR_H

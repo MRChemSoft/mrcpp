42d0af5b7f996f779b2caea2dd4cdfc93fb49cde
Stig Rune Jensen <stig.r.jensen@uit.no>
2016-11-23 13:05:57 +0100

    read 1st and 2nd order PH derivative operators from file

--- mwoperators/PHCalculator.h
+++ mwoperators/PHCalculator.h
@@ -7,16 +7,17 @@
 
 class PHCalculator : public TreeCalculator<2> {
 public:
-    PHCalculator(const ScalingBasis &basis, int order);
+    PHCalculator(const ScalingBasis &basis, int n);
     virtual ~PHCalculator() { }
 
 protected:
+    const int diff_order;
     Eigen::MatrixXd S_m1;
     Eigen::MatrixXd S_0;
     Eigen::MatrixXd S_p1;
 
     virtual void calcNode(MWNode<2> &node);
-    void readSMatrix(const ScalingBasis &basis);
+    void readSMatrix(const ScalingBasis &basis, char n);
 };
 
 #endif // PHCALCULATOR_H

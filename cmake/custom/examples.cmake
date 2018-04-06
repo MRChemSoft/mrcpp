cb5199d37e5bf9dff893d77da5485dfb66e229d7
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-04-06 14:52:36 +0200

    Bump version CMake3.3 and Eigen3.1.0

--- cmake/custom/examples.cmake
+++ cmake/custom/examples.cmake
@@ -1,3 +1,10 @@
+#.rst:
+#
+# autocmake.yml configuration::
+#
+#   docopt: "--enable-examples Enable code examples [default: False]."
+#   define: "'-DENABLE_EXAMPLES={0}'.format(arguments['--enable-examples'])"
+
 if(ENABLE_EXAMPLES)
   add_subdirectory(examples)
 endif()

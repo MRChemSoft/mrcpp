3e2c473cbf05b3493c2b75ded101325cd9792053
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-04-10 20:25:07 +0200

    Move mefilters to share

--- cmake/custom/mw_filters.cmake
+++ cmake/custom/mw_filters.cmake
@@ -1,4 +1,12 @@
 if(NOT DEFINED ${MW_FILTER_DIR})
-    set(MW_FILTER_DIR "${PROJECT_SOURCE_DIR}/src/mwfilters" CACHE STRING "Path to MW filters and cross-correlation coefs")
+    set(MW_FILTER_DIR "${CMAKE_SOURCE_DIR}/share/mwfilters" CACHE STRING "Path to MW filters and cross-correlation coefs")
 endif()
 
+option(MRCPP_INSTALL_FILTERS "Install MW filters" ON)
+
+if(MRCPP_INSTALL_FILTERS)
+    install(DIRECTORY ${MW_FILTER_DIR}
+        DESTINATION share/${PROJECT_NAME}/mwfilters
+        )
+    set(MW_FILTER_DIR ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/mwfilters)
+endif()

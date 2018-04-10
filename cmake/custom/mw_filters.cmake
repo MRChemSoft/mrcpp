bbd229e3089237b1c7f5aaad194a67f73a00e03f
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-04-10 20:59:17 +0200

    Set INSTALL_FILTERS OFF

--- cmake/custom/mw_filters.cmake
+++ cmake/custom/mw_filters.cmake
@@ -2,7 +2,7 @@ if(NOT DEFINED ${MW_FILTER_DIR})
     set(MW_FILTER_DIR "${CMAKE_SOURCE_DIR}/share/mwfilters" CACHE STRING "Path to MW filters and cross-correlation coefs")
 endif()
 
-option(MRCPP_INSTALL_FILTERS "Install MW filters" ON)
+option(MRCPP_INSTALL_FILTERS "Install MW filters" OFF)
 
 if(MRCPP_INSTALL_FILTERS)
     install(DIRECTORY ${MW_FILTER_DIR}

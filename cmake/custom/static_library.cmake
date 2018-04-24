a51b84e3a8b1df4912a386ef5e26d4f47aea60ba
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-24 09:01:57 -0400

    Update Autocmake, fix options wrappers

--- cmake/custom/static_library.cmake
+++ cmake/custom/static_library.cmake
@@ -7,20 +7,5 @@
 #   docopt: "--static Create only the static library [default: False]."
 #   define: "'-DSTATIC_LIBRARY_ONLY={0}'.format(arguments['--static'])"
 
-
-option_with_print(
-  NAME
-    STATIC_LIBRARY_ONLY
-  MESSAGE
-    "Create the static library only"
-  DEFAULT
-    OFF
-  )
-option_with_print(
-  NAME
-    SHARED_LIBRARY_ONLY
-  MESSAGE
-    "Create the shared library only"
-  DEFAULT
-    OFF
-  )
+option_with_print(STATIC_LIBRARY_ONLY "Create the static library only" OFF)
+option_with_print(SHARED_LIBRARY_ONLY "Create the shared library only" OFF)

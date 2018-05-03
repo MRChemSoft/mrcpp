870adbd64f32aa5723c3543b2b4fa04bd4724b44
Magnar Bjørgve <magnbjor@gmail.com>
2018-05-03 10:27:53 +0200

    Add projection/integral test

--- pymrcpp/test_python.py
+++ pymrcpp/test_python.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pymrcpp as mr 
 
+from math import isclose
+
 min_scale = -4
 max_depth = 25 
 order = 7
@@ -14,6 +16,20 @@ world = mr.BoundingBox3D(min_scale, corner, boxes)
 basis = mr.InterpolatingBasis(order)
 
 MRA = mr.MultiResolutionAnalysis3D(world, basis, max_depth)
+
+def phi_exact(x, y, z):
+    beta = 100
+    alpha = (beta/np.pi)**(3/2)
+
+    return alpha*np.exp(-beta*(x**2 + y**2 + z**2))
+
+
+phi_tree = mr.FunctionTree3D(MRA)
+mr.project(prec, phi_tree, phi_exact)
+
+
+def test_IsIntWorking():
+    assert isclose(1.0, phi_tree.integrate(), rel_tol = prec)
 
 def test_BBGetScale():
     assert world.getScale() == min_scale

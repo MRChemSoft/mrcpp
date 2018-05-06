eaae0e8d0435324c87d3622e4e25f9850a2211d4
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-05 23:24:23 -0400

    Add an INSTALL_INTERFACE include

--- cmake/custom/main.cmake
+++ cmake/custom/main.cmake
@@ -3,14 +3,14 @@
 set(CMAKECONFIG_INSTALL_DIR "share/cmake/${PROJECT_NAME}")
 
 configure_file (
-    "${PROJECT_SOURCE_DIR}/config.h.in"
-    "${PROJECT_BINARY_DIR}/config.h"
-    )
+  ${PROJECT_SOURCE_DIR}/config.h.in
+  ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_INCLUDEDIR}/config.h
+  )
 
 install(
-    FILES ${CMAKE_BINARY_DIR}/config.h
-    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME}
-    )
+  FILES ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_INCLUDEDIR}/config.h
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME}
+  )
 
 add_subdirectory(src)
 add_subdirectory(include)

4782c4c177a41efcdb917c172e8aeb225bb77e2e
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-15 11:58:48 -0400

    Forward declarations instead of inclusions

--- src/treebuilders/multiply.h
+++ src/treebuilders/multiply.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#include "functions/RepresentableFunction.h"
-#include "trees/FunctionTree.h"
 #include "trees/FunctionTreeVector.h"
 
 namespace mrcpp {
+template <int D> class RepresentableFunction;
+template <int D> class FunctionTree;
+
 template<int D> void multiply(double prec, FunctionTree<D> &out, double c, FunctionTree<D> &tree_a, FunctionTree<D> &tree_b, int maxIter = -1);
 template<int D> void multiply(double prec, FunctionTree<D> &out, FunctionTreeVector<D> &inp, int maxIter = -1);
 template<int D> void power(double prec, FunctionTree<D> &out, FunctionTree<D> &inp, double pow);

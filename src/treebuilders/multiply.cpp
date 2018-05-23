bddac83638630808e3baec5fe7e60a1abeba9f82
Stig Rune Jensen <stig.r.jensen@uit.no>
2018-05-23 22:59:24 +0200

    Add dot product of FunctionTreeVectors

--- src/treebuilders/multiply.cpp
+++ src/treebuilders/multiply.cpp
@@ -1,8 +1,12 @@
 #include "multiply.h"
+#include "add.h"
+#include "grid.h"
 #include "TreeBuilder.h"
 #include "WaveletAdaptor.h"
 #include "MultiplicationCalculator.h"
+#include "trees/HilbertIterator.h"
 #include "trees/FunctionTree.h"
+#include "trees/FunctionNode.h"
 #include "utils/Printer.h"
 #include "utils/Timer.h"
 
@@ -12,13 +16,13 @@ template<int D>
 void multiply(double prec,
               FunctionTree<D> &out,
               double c,
-              FunctionTree<D> &tree_a,
-              FunctionTree<D> &tree_b,
+              FunctionTree<D> &inp_a,
+              FunctionTree<D> &inp_b,
               int maxIter) {
-    FunctionTreeVector<D> tree_vec;
-    tree_vec.push_back(std::make_tuple(c, &tree_a));
-    tree_vec.push_back(std::make_tuple(1.0, &tree_b));
-    mrcpp::multiply(prec, out, tree_vec, maxIter);
+    FunctionTreeVector<D> tmp_vec;
+    tmp_vec.push_back(std::make_tuple(c, &inp_a));
+    tmp_vec.push_back(std::make_tuple(1.0, &inp_b));
+    multiply(prec, out, tmp_vec, maxIter);
 }
 
 template<int D>
@@ -60,6 +64,28 @@ void map(double prec, FunctionTree<D> &out, FunctionTree<D> &inp, RepresentableF
     NOT_IMPLEMENTED_ABORT;
 }
 
+template<int D>
+void dot(double prec, FunctionTree<D> &out, FunctionTreeVector<D> &inp_a, FunctionTreeVector<D> &inp_b, int maxIter) {
+    if (inp_a.size() != inp_b.size()) MSG_FATAL("Input length mismatch");
+
+    FunctionTreeVector<D> tmp_vec;
+    for (int d = 0; d < inp_a.size(); d++) {
+        double coef_a = get_coef(inp_a, d);
+        double coef_b = get_coef(inp_b, d);
+        FunctionTree<D> &tree_a = get_func(inp_a, d);
+        FunctionTree<D> &tree_b = get_func(inp_b, d);
+        if (out.getMRA() != tree_a.getMRA()) MSG_FATAL("Trees not compatible");
+        if (out.getMRA() != tree_b.getMRA()) MSG_FATAL("Trees not compatible");
+        FunctionTree<D> *out_d = new FunctionTree<D>(out.getMRA());
+        copy_grid(*out_d, out);
+        multiply(prec, *out_d, 1.0, tree_a, tree_b, maxIter);
+        tmp_vec.push_back(std::make_tuple(coef_a*coef_b, out_d));
+    }
+    copy_grid(out, tmp_vec);
+    add(-1.0, out, tmp_vec, 0);
+    clear(tmp_vec, true);
+}
+
 template void multiply(double prec, FunctionTree<1> &out, double c, FunctionTree<1> &tree_a, FunctionTree<1> &tree_b, int maxIter);
 template void multiply(double prec, FunctionTree<2> &out, double c, FunctionTree<2> &tree_a, FunctionTree<2> &tree_b, int maxIter);
 template void multiply(double prec, FunctionTree<3> &out, double c, FunctionTree<3> &tree_a, FunctionTree<3> &tree_b, int maxIter);
@@ -72,5 +98,8 @@ template void power(double prec, FunctionTree<3> &out, FunctionTree<3> &tree, do
 template void map(double prec, FunctionTree<1> &out, FunctionTree<1> &inp, RepresentableFunction<1> &func);
 template void map(double prec, FunctionTree<2> &out, FunctionTree<2> &inp, RepresentableFunction<2> &func);
 template void map(double prec, FunctionTree<3> &out, FunctionTree<3> &inp, RepresentableFunction<3> &func);
+template void dot(double prec, FunctionTree<1> &out, FunctionTreeVector<1> &inp_a, FunctionTreeVector<1> &inp_b, int maxIter);
+template void dot(double prec, FunctionTree<2> &out, FunctionTreeVector<2> &inp_a, FunctionTreeVector<2> &inp_b, int maxIter);
+template void dot(double prec, FunctionTree<3> &out, FunctionTreeVector<3> &inp_a, FunctionTreeVector<3> &inp_b, int maxIter);
 
 } //namespace mrcpp

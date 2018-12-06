7fff9968f17eefa61b51043b0ccae41e0ffce6ed
Magnar Bjørgve <magnbjor@gmail.com>
2018-12-06 15:31:47 +0100

    Fix sloppy work

--- cmake/compiler_flags/Intel.CXX.cmake
+++ cmake/compiler_flags/Intel.CXX.cmake
@@ -1,6 +1,6 @@
 if(NOT DEFINED ENV{CXXFLAGS})
     if(CMAKE_CXX_COMPILER_ID MATCHES Intel)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-pragmas -std=c++14")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-pragmas")
         set(CMAKE_CXX_FLAGS_RELEASE "-debug -O3 -DNDEBUG")
         set(CMAKE_CXX_FLAGS_DEBUG "-O0 -debug -DDEBUG")
     endif()

a181b9b59f7d8df367fbdd1f5cc06be41c08de3c
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-01-29 14:40:51 +0100

    Use -O3/-O2 rather than -Ofast

--- cmake/compiler_flags/Clang.CXX.cmake
+++ cmake/compiler_flags/Clang.CXX.cmake
@@ -1,7 +1,7 @@
 if(NOT DEFINED ENV{CXXFLAGS})
     if(CMAKE_CXX_COMPILER_ID MATCHES Clang)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-padded -Wno-c++98-compat -Wno-c++11-compat -Wno-unknown-pragmas -Woverloaded-virtual -Wwrite-strings")
-        set(CMAKE_CXX_FLAGS_RELEASE "-Ofast -DNDEBUG")
+        set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
         set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3 -DDEBUG")
     endif()
 endif()

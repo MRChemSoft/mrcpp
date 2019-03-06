0f5e209a79e0060c370d068ab7ca32fea446c5bc
Stig Rune Jensen <stig.r.jensen@uit.no>
2019-03-06 13:34:02 +0100

    Add separate build_grid for GaussExp

--- src/treebuilders/grid.h
+++ src/treebuilders/grid.h
@@ -30,6 +30,7 @@
 #include "trees/FunctionTreeVector.h"
 
 namespace mrcpp {
+template <int D> void build_grid(FunctionTree<D> &out, const GaussExp<D> &inp, int maxIter = -1);
 template <int D> void build_grid(FunctionTree<D> &out, const RepresentableFunction<D> &inp, int maxIter = -1);
 template <int D> void build_grid(FunctionTree<D> &out, FunctionTree<D> &inp, int maxIter = -1);
 template <int D> void build_grid(FunctionTree<D> &out, FunctionTreeVector<D> &inp, int maxIter = -1);

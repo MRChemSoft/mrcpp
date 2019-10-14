8e70685c0f2b2a0b0a0c0ce6617351ae4d1e6023
Magnar Bjørgve <magnbjor@gmail.com>
2019-10-14 14:32:18 +0200

    Make bear bones conda for mrcpp
    Co-authored-by: Roberto <roberto.diremigio@gmail.com>

---
+++ conda-recipes/mrcpp/build.sh
@@ -0,0 +1,29 @@
+#    # load Intel compilers
+#    set +x
+#    source /theoryfs2/common/software/intel2018/bin/compilervars.sh intel64
+#    set -x
+# error with Intel 2018.3, 2019.4
+#
+#    ALLOPTS="-gnu-prefix=${HOST}- ${OPTS}"
+
+# configure
+${BUILD_PREFIX}/bin/cmake \
+        -H${SRC_DIR} \
+        -Bbuild \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_COMPILER=${CXX} \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DBUILD_STATIC_LIBS=False \
+        -DENABLE_TESTS=True \
+        -G"Ninja"
+
+# build
+cd build
+ninja -j${CPU_COUNT}
+
+# test
+ctest -j${CPU_COUNT} --output-on-failure --verbose
+
+# install
+ninja install

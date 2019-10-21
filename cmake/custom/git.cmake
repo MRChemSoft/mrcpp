51ebdc2e7333df366104e951b71105ab81275d2f
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-10-21 17:30:30 +0200

    Finesse fix for git.cmake

--- cmake/custom/git.cmake
+++ cmake/custom/git.cmake
@@ -1,14 +1,13 @@
 set(GIT_REVISION "undefined")
-find_package(Git)
-
+find_package(Git QUIET)
 if(GIT_FOUND)
   execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit --max-count=1 HEAD
     OUTPUT_VARIABLE _git_revision
+    ERROR_QUIET
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     )
-  string(STRIP ${_git_revision} _git_revision)
   if(_git_revision)
-    set(GIT_REVISION ${_git_revision})
+    string(STRIP ${_git_revision} GIT_REVISION)
   endif()
 endif()

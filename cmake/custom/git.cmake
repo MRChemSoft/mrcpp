f519379fa77c44add2816beb4306ca1965485e25
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-10-21 17:23:13 +0200

    Fix git.cmake when building from tarball

--- cmake/custom/git.cmake
+++ cmake/custom/git.cmake
@@ -1,13 +1,14 @@
-set(GIT_REVISION)
+set(GIT_REVISION "undefined")
 find_package(Git)
 
 if(GIT_FOUND)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit --max-count=1 HEAD
-        OUTPUT_VARIABLE GIT_REVISION
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        )
-    string(STRIP
-        ${GIT_REVISION}
-        GIT_REVISION)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-list --abbrev-commit --max-count=1 HEAD
+    OUTPUT_VARIABLE _git_revision
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    )
+  string(STRIP ${_git_revision} _git_revision)
+  if(_git_revision)
+    set(GIT_REVISION ${_git_revision})
+  endif()
 endif()

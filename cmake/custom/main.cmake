1f59034feb4d8ad472f36b64af51830d280f1f7f
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-10-30 10:38:24 +0100

    Save work

--- cmake/custom/main.cmake
+++ cmake/custom/main.cmake
@@ -4,7 +4,6 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 # Hardcode to share, rather than use CMAKE_INSTALL_DATAROOTDIR as the latter
 # might resolve to a place not recognized by CMake
 set(CMAKECONFIG_INSTALL_DIR "share/cmake/${PROJECT_NAME}")
-add_subdirectory(share)
 
 configure_file (
   ${PROJECT_SOURCE_DIR}/config.h.in
@@ -16,6 +15,7 @@ list(APPEND mrcpp_headers ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_INCLUDEDIR}/${PR
 
 include(${PROJECT_SOURCE_DIR}/external/upstream/fetch_eigen3.cmake)
 
+add_subdirectory(share)
 add_subdirectory(api)
 add_subdirectory(src)
 add_subdirectory(pilot)

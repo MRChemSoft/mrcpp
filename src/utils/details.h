63e15bb7a852fbad67ca4c5c783c6b89492b61ba
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-11-04 20:48:59 +0100

    Make the overload of operator<< actually work

--- src/utils/details.h
+++ src/utils/details.h
@@ -23,23 +23,38 @@
  * <https://mrcpp.readthedocs.io/>
  */
 
+#pragma once
+
 #include <array>
 #include <iostream>
+#include <sstream>
 
 namespace mrcpp {
- 
-template <typename T, int D> inline auto operator<<(std::ostream &os, const std::array<T, D> &coord) -> std::ostream & {
-    os << "[";
-    for (auto elem : coord) { os << elem << ", "; }
-    os << "]";
-    return os;
-}
-  
 namespace details {
-
 int get_memory_usage();
 template <int D> bool are_all_equal(const std::array<double, D> &exponent);
 template <typename T, int D> std::array<T, D> convert_to_std_array(T *arr);
-
+template <typename T> auto stream_collection(const T &coll) -> std::string {
+    std::ostringstream os;
+    os << "[";
+    for (auto elem : coll) {
+        os << elem;
+        if (elem != coll.back()) os << ", ";
+    }
+    os << "]";
+    return os.str();
+}
 } // namespace details
+
+template <typename T> auto operator<<(std::ostream &os, const std::array<T, 1> &coll) -> std::ostream & {
+    return (os << details::stream_collection(coll));
+}
+
+template <typename T> auto operator<<(std::ostream &os, const std::array<T, 2> &coll) -> std::ostream & {
+    return (os << details::stream_collection(coll));
+}
+
+template <typename T> auto operator<<(std::ostream &os, const std::array<T, 3> &coll) -> std::ostream & {
+    return (os << details::stream_collection(coll));
+}
 } // namespace mrcpp

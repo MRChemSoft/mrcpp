49db92a35b342214a8314304b3484bffe62c1fc0
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-12-12 12:32:12 +0100

    Add git describe

--- src/utils/Printer.cpp
+++ src/utils/Printer.cpp
@@ -93,9 +93,10 @@ void print::environment(int level) {
     printout(level, std::endl);
     print::separator(level, '-', 1);
     println(level, " MRCPP version         : " << program_version());
-    println(level, " Git commit hash       : " << git_commit_hash() << std::endl);
-    println(level, " Git commit author     : " << git_commit_author() << std::endl);
-    println(level, " Git commit date       : " << git_commit_date() << std::endl);
+    println(level, " Git describe          : " << git_describe());
+    println(level, " Git commit hash       : " << git_commit_hash());
+    println(level, " Git commit author     : " << git_commit_author());
+    println(level, " Git commit date       : " << git_commit_date());
     println(level, " Git branch            : " << git_branch() << std::endl);
 
 #ifdef HAVE_BLAS

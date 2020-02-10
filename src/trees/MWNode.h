21172f97fd991c766cc53ff13768440edc7284e7
Peter Wind <peter.wind@met.no>
2020-02-10 15:11:07 +0100

    clang format

--- src/trees/MWNode.h
+++ src/trees/MWNode.h
@@ -163,8 +163,8 @@ protected:
     double maxSquareNorm;          // Largest squared norm among itself and descendants.
     double maxWSquareNorm;         // Largest wavelet squared norm among itself and descendants.
                                    // NB: must be set before used.
-                           // NB2: normalization is such that a constant function gives constant value, i.e. *not* same
-                           // normalization as squareNorm
+    // NB2: normalization is such that a constant function gives constant value, i.e. *not* same
+    // normalization as squareNorm
     void setMaxSquareNorm(); // recursively set maxSquaredNorm and maxWSquareNorm of parent and descendants
     void
     resetMaxSquareNorm(); // recursively reset maxSquaredNorm  and maxWSquareNorm of parent and descendants to value -1

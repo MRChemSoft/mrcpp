ba2ff049edc31e3e5129bbc6c80281406f581d09
Roberto Di Remigio <robertodr@users.noreply.github.com>
2020-09-22 19:20:56 +0200

    Export compiler flags with MRCPP CMake target (#147)
    * Export compiler flags with MRCPP CMake target
    
    * Formatting

--- cmake/compiler_flags/CXXFlags.cmake
+++ cmake/compiler_flags/CXXFlags.cmake
@@ -6,3 +6,7 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
 include(${CMAKE_CURRENT_LIST_DIR}/GNU.CXX.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Intel.CXX.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Clang.CXX.cmake)
+
+string(REPLACE " " ";" _cmake_cxx_flags ${CMAKE_CXX_FLAGS})
+string(REPLACE " " ";" _cmake_cxx_flags_release ${CMAKE_CXX_FLAGS_RELEASE})
+string(REPLACE " " ";" _cmake_cxx_flags_relwithdebinfo ${CMAKE_CXX_FLAGS_RELWITHDEBINFO})

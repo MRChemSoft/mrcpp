29ee4ebf9b53efbb679279fe5e494150e2a10e85
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-09-26 16:06:40 +0200

    Allow to remove arch-specific optimization flags

--- cmake/compiler_flags/CXXFlags.cmake
+++ cmake/compiler_flags/CXXFlags.cmake
@@ -1,7 +1,33 @@
+#.rst:
+#
+# Enables architecture-specific compiler flags.
+#
+# Variables used::
+#
+#   ENABLE_ARCH_FLAGS
+#
+# autocmake.yml configuration::
+#
+#   docopt: "--arch-flags Enable architecture-specific compiler flags [default: True]."
+#   define: "'-DENABLE_ARCH_FLAGS={0}'.format(arguments['--arch-flags'])"
+
+option(ENABLE_ARCH_FLAGS "Enable architecture-specific compiler flags" ON)
+
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
 set(CMAKE_CXX_EXTENSIONS FALSE)
 set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
+
+if(ENABLE_ARCH_FLAGS)
+  include(set_compiler_flag.cmake)
+  # iterate over list of flags and use the first one that is compatible with the
+  # compiler in use
+  set_compiler_flag(_arch_flag
+    "-march=native"  # valid wth GNU and probably Clang too
+    "-xHost"  # valid with Intel
+    )
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${_arch_flag}")
+endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/GNU.CXX.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Intel.CXX.cmake)

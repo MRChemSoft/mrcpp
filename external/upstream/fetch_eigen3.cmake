0ab8bc06cfa1206b3f616b36a99398043ad70486
Stig Rune Jensen <stig.r.jensen@uit.no>
2021-06-24 10:21:54 +0200

    Import fetch_eigen from MRChem (#172)

--- external/upstream/fetch_eigen3.cmake
+++ external/upstream/fetch_eigen3.cmake
@@ -1,4 +1,7 @@
-find_package(Eigen3 3.3 CONFIG QUIET NO_CMAKE_PACKAGE_REGISTRY)
+find_package(Eigen3 3.3 CONFIG QUIET
+  NO_CMAKE_PATH
+  NO_CMAKE_PACKAGE_REGISTRY
+  )
 if(TARGET Eigen3::Eigen)
   message(STATUS "Using Eigen3: ${EIGEN3_ROOT_DIR} (version ${Eigen3_VERSION})")
 else()
@@ -8,12 +11,14 @@ else()
   FetchContent_Declare(eigen3_sources
     QUIET
     URL
-      https://gitlab.com/libeigen/eigen/-/archive/before-git-migration/eigen-before-git-migration.tar.gz
-   )
+      https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz
+    PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/eigen-config-cmake.patch
+    )
 
   FetchContent_GetProperties(eigen3_sources)
-  
+
   set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+
   if(NOT eigen3_sources_POPULATED)
     FetchContent_Populate(eigen3_sources)
 

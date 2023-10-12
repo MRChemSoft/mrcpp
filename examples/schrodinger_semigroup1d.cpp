6cb373df50004bacc403fbf1db28b78387a48092
edinvay <evgeni36@yandex.ru>
2023-10-12 14:31:05 +0200

    Prepared example on the analysis of the semigroup with ploting python scripts in tools/

--- examples/schrodinger_semigroup1d.cpp
+++ examples/schrodinger_semigroup1d.cpp
@@ -1,20 +1,48 @@
 #include "MRCPP/MWFunctions"
 #include <MRCPP/MWOperators>
 #include <MRCPP/Printer>
+#include "MRCPP/Plotter"
 #include <MRCPP/Timer>
 #include "operators/TimeEvolutionOperator.h"
 #include "functions/special_functions.h"
+#include "treebuilders/complex_apply.h"
 
 
-
-//std::complex<double> free_particle_analytical_solution(double x, double x0, double t, double sigma);
 
 const auto min_scale = 0;
 const auto max_depth = 25;
 
-const auto order = 7;
+const auto order = 4;
 const auto prec = 1.0e-7;
 
+int finest_scale = 10; //for time evolution operator construction (not recommended to use more than 10)
+int max_Jpower = 20;  //the amount of J integrals to be used in construction (20 should be enough)
+
+// Time moments:
+double t1 = 0.001;         //initial time moment (not recommended to use more than 0.001)
+double delta_t = 0.001;    //time step (not recommended to use less than 0.001)
+double t2 = delta_t + t1;  //final time moment
+
+    
+/**
+ * @brief Exploring free-particle time evolution.
+ * @details We check the time propagator.
+ *
+ * The time evolution equation is given by:
+ * \f[
+ *   g(x) = \exp \left( i \delta t \partial_x^2 \right) f(x)
+ * \f]
+ *
+ * where \f$f(x) = \psi(x, t_1)\f$,
+ * \f$g(x) = \psi(x, t_2)\f$,
+ * \f$\delta t = t_2 - t_1\f$
+ * and
+ * \f[
+ *   \psi(x, t) = \sqrt{\frac{\sigma}{4it + \sigma}} e^{-\frac{(x - x_0)^2}{4it + \sigma}}
+ *   .
+ * \f]
+ * 
+ */
 int main(int argc, char **argv)
 {
     auto timer = mrcpp::Timer();
@@ -23,124 +51,110 @@ int main(int argc, char **argv)
     auto printlevel = 0;
     mrcpp::Printer::init(printlevel);
     mrcpp::print::environment(0);
-    mrcpp::print::header(0, "Building operator");
-
+    
     // Initialize world in the unit cube [0,1]
     auto basis = mrcpp::LegendreBasis(order);
     auto world = mrcpp::BoundingBox<1>(min_scale);
     auto MRA = mrcpp::MultiResolutionAnalysis<1>(world, basis, max_depth);
 
+    mrcpp::print::header(0, "Building operator");
     mrcpp::print::footer(0, timer, 2);
 
-    // Time moment:
-    double t = 0.001;
-    int finest_scale = 8;
-
-    mrcpp::TimeEvolutionOperator<1> ReExp(MRA, prec, t, finest_scale, false);
-    //Exp.initialize(4);  //impossible to initialise outside of a constructor!?
+    // Time evolution operatror Exp(delta_t)
+    mrcpp::TimeEvolutionOperator<1> ReExp(MRA, prec, delta_t, finest_scale, false);
     println(0, ReExp.getComponent(0, 0));
-
-    mrcpp::TimeEvolutionOperator<1> ImExp(MRA, prec, t, finest_scale, true);
+    mrcpp::TimeEvolutionOperator<1> ImExp(MRA, prec, delta_t, finest_scale, true);
     println(0, ImExp.getComponent(0, 0));
 
-    //double x = 1.5;  // Example value for x
-    //double result = std::erf(x);
-    //std::cout << "erf(" << x << ") = " << result << std::endl;
+    mrcpp::print::header(0, "Preparing analytical solution");
+    mrcpp::print::footer(0, timer, 2);
 
-    // Analytical solution
-    double sigma = t;
+    // Analytical solution parameters for psi(x, t)
+    double sigma = 0.001;
     double x0 = 0.5;
-    auto Re_f = [sigma, x0, t=0](const mrcpp::Coord<1> &r) -> double
+
+    // Functions f(x) = psi(x, t1) and g(x) = psi(x, t2)
+    auto Re_f = [sigma, x0, t=t1](const mrcpp::Coord<1> &r) -> double
     {
         return mrcpp::free_particle_analytical_solution(r[0], x0, t, sigma).real();
     };
-    /*
-    auto Im_f = [sigma, x0, t=0](const mrcpp::Coord<1> &r) -> double
+    auto Im_f = [sigma, x0, t=t1](const mrcpp::Coord<1> &r) -> double
     {
         return mrcpp::free_particle_analytical_solution(r[0], x0, t, sigma).imag();
     };
-    */
-//    std::cout << "t = " << t << std::endl;
-    auto Re_g = [sigma, x0, t](const mrcpp::Coord<1> &r) -> double
+    auto Re_g = [sigma, x0, t=t2](const mrcpp::Coord<1> &r) -> double
     {
         return mrcpp::free_particle_analytical_solution(r[0], x0, t, sigma).real();
     };
-    auto Im_g = [sigma, x0, t](const mrcpp::Coord<1> &r) -> double
+    auto Im_g = [sigma, x0, t=t2](const mrcpp::Coord<1> &r) -> double
     {
         return mrcpp::free_particle_analytical_solution(r[0], x0, t, sigma).imag();
     };
-    // Test the functions
-    /*
-    for( double x = 0.0; x < 1.0; x += 0.1 )
-        std::cout << "f(" << x << ") = " << Re_f({x}) << std::endl;
-    for( double x = 0.0; x < 1.0; x += 0.1 )
-        std::cout << "g(" << x << ") = " << Re_g({x}) << std::endl;
-    */
+
     // Projecting functions
     mrcpp::FunctionTree<1> Re_f_tree(MRA);
-    mrcpp::project<1>(prec, Re_f_tree, Re_f, -1);
-//    auto integral = Re_f_tree.integrate();
+    mrcpp::project<1>(prec, Re_f_tree, Re_f);
+    mrcpp::FunctionTree<1> Im_f_tree(MRA);
+    mrcpp::project<1>(prec, Im_f_tree, Im_f);
     mrcpp::FunctionTree<1> Re_g_tree(MRA);
-    mrcpp::project<1>(prec, Re_g_tree, Re_g, -1);
+    mrcpp::project<1>(prec, Re_g_tree, Re_g);
     mrcpp::FunctionTree<1> Im_g_tree(MRA);
-    mrcpp::project<1>(prec, Im_g_tree, Im_g, -1);
+    mrcpp::project<1>(prec, Im_g_tree, Im_g);
 
-    //std::cout << Re_f_tree;
-    //std::cout << Re_g_tree;
-    //std::cout << Im_g_tree;
-
+    // Output function trees
     mrcpp::FunctionTree<1> Re_fout_tree(MRA);
     mrcpp::FunctionTree<1> Im_fout_tree(MRA);
-    //mrcpp::FunctionTree<1> temp_fout_tree(MRA);
-    mrcpp::apply(prec, Re_fout_tree, ReExp, Re_f_tree);
-    //mrcpp::apply(prec, temp_fout_tree, ImExp, Im_f_tree);
-    //Re_fout_tree -= temp_fout_tree;
-    mrcpp::apply(prec, Im_fout_tree, ImExp, Re_f_tree);
-    //... in case Im_f is not zero
+    
+    // Complex objects for use in apply()
+    mrcpp::ComplexObject< mrcpp::ConvolutionOperator<1> > E(ReExp, ImExp);
+    mrcpp::ComplexObject< mrcpp::FunctionTree<1> > input(Re_f_tree, Im_f_tree);
+    mrcpp::ComplexObject< mrcpp::FunctionTree<1> > output(Re_fout_tree, Im_fout_tree);
 
+    mrcpp::print::header(0, "Applying operator");
+    mrcpp::print::footer(0, timer, 2);
+
+    // Apply operator Exp(delta_t) f(x)
+    mrcpp::apply(prec, output, E, input);
+    
+    mrcpp::print::header(0, "Checking the result on analytical solution");
+    mrcpp::print::footer(0, timer, 2);
+
+    // Check g(x) = Exp(delta_t) f(x)
     mrcpp::FunctionTree<1> Re_error(MRA);  // = Re_fout_tree - Re_g_tree
     mrcpp::FunctionTree<1> Im_error(MRA);  // = Im_fout_tree - Im_g_tree
     
     // Re_error = Re_fout_tree - Re_g_tree
-    auto Re_f_vec = mrcpp::FunctionTreeVector<1>();
-    Re_f_vec.push_back(std::make_tuple(1.0, &Re_fout_tree));
-    Re_f_vec.push_back(std::make_tuple(-1.0, &Re_g_tree));
-    mrcpp::add(prec, Re_error, Re_f_vec);
-
+    add(prec, Re_error, 1.0, Re_fout_tree, -1.0, Re_g_tree);
     auto Re_integral = Re_error.integrate();
     auto Re_sq_norm = Re_error.getSquareNorm();
-    mrcpp::print::value(0, "Integral", Re_integral);
-    mrcpp::print::value(0, "Square norm", Re_sq_norm);
+    mrcpp::print::value(0, "Integral of    Re(Exp(delta_t) f(x) - g(x)) =", Re_integral);
+    mrcpp::print::value(0, "Square norm of Re(Exp(delta_t) f(x) - g(x)) =", Re_sq_norm);
     
     // Im_error = Im_fout_tree - Im_g_tree
-    auto Im_f_vec = mrcpp::FunctionTreeVector<1>();
-    Im_f_vec.push_back(std::make_tuple(1.0, &Im_fout_tree));
-    Im_f_vec.push_back(std::make_tuple(-1.0, &Im_g_tree));
-    mrcpp::add(prec, Im_error, Im_f_vec);
-
+    add(prec, Im_error, 1.0, Im_fout_tree, -1.0, Im_g_tree);
     auto Im_integral = Im_error.integrate();
     auto Im_sq_norm = Im_error.getSquareNorm();
-    mrcpp::print::value(0, "Integral", Im_integral);
-    mrcpp::print::value(0, "Square norm", Im_sq_norm);
-    
-    //std::cout << Re_error;
-    //Re_error.crop(prec);
-    //std::cout << Re_error;
-    
-    
+    mrcpp::print::value(0, "Integral of    Im(Exp(delta_t) f(x) - g(x)) =", Im_integral);
+    mrcpp::print::value(0, "Square norm of Im(Exp(delta_t) f(x) - g(x)) =", Im_sq_norm);
+        
+    mrcpp::print::header(0, "Saving plots to files");
+    mrcpp::print::footer(0, timer, 2);
 
+    // Set plotting parameters
+    int nPts = 1000;                                     
+    mrcpp::Coord<1> o{0.0};                   
+    mrcpp::Coord<1> a{1.0};                   
+    mrcpp::Plotter<1> plot(o);                           
+    plot.setRange(a);
+
+    plot.linePlot({nPts}, Re_error, "Re_error");             // Write to file Re_error.line
+    plot.linePlot({nPts}, Im_error, "Im_error");             // Write to file Im_error.line
+    plot.linePlot({nPts}, Re_f_tree, "Re_f_tree");           // Write to file Re_f_tree.line
+    plot.linePlot({nPts}, Im_f_tree, "Im_f_tree");           // Write to file Im_f_tree.line
+    plot.linePlot({nPts}, Re_g_tree, "Re_g_tree");           // Write to file Re_g_tree.line
+    plot.linePlot({nPts}, Im_g_tree, "Im_g_tree");           // Write to file Im_g_tree.line
+    
     mrcpp::print::footer(0, timer, 2);
     return 0;
 }
 
-/*
-std::complex<double> free_particle_analytical_solution(double x, double x0, double t, double sigma)
-{
-    std::complex<double> i(0.0, 1.0);  // Imaginary unit
-    auto denominator = 4 * t * i + sigma;
-    std::complex<double> sqrt_denom = std::sqrt(denominator);
-    std::complex<double> exponent = -((x - x0) * (x - x0)) / denominator;
-
-    return std::sqrt(sigma) / sqrt_denom * std::exp(exponent);
-}
-*/

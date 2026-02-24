acda6506ae4f054114f0b8ddc036767e77a77750
Niklas Göllmann <ngoel@uan02.head.cm.americas.sgi.com>
2026-02-24 13:26:14 +0100

    added compile scripts for Saga, Betzy and Olivia, similar to those for MRChem

---
+++ tools/olivia.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+
+if [ -z "$1" ]; then
+    # If not, prompt the user for input
+    echo "This script requires an NRIS account for compilation on a compute node"
+    read -p "Please state your account (nnxxxxk): " account
+else
+    # Otherwise, use the argument
+    account="$1"
+fi
+
+mrcpp_dir="$(pwd)"
+source ${mrcpp_dir}/tools/olivia.env
+
+cd ${mrcpp_dir}
+build_dir=${mrcpp_dir}/build
+install_dir=${mrcpp_dir}/install
+
+if [ -d "${build_dir}" ]; then
+    echo "Build directory already exists, please remove"
+    exit 1
+else
+    ./setup --prefix=${install_dir} --omp --mpi ${build_dir} && \
+    cd ${build_dir} && \
+    srun --cpus-per-task=4 --mem-per-cpu=2G --time=01:00:00 --account=$account bash -c \
+    "source ${mrcpp_dir}/tools/olivia.env ; \
+    make -j4 ; \
+    OMP_NUM_THREADS=4 ctest -L unit --output-on-failure ; \
+    make install -j4"
+fi
+
+exit 0
---
+++ tools/saga.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+if [ `hostname | grep -i login | wc -l` == 0 ]; then
+   echo "This script MUST be run on the LOGIN node as it requires internet access"
+   exit 1
+fi
+
+mrcpp_dir="$(pwd)"
+source ${mrcpp_dir}/tools/saga.env
+
+cd ${mrcpp_dir}
+build_dir=${mrcpp_dir}/build
+install_dir=${mrcpp_dir}/install
+
+if [ -d "${build_dir}" ]; then
+    echo "Build directory already exists, please remove"
+    exit 1
+else
+    ./setup --prefix=${install_dir} --omp --mpi --cxx=mpicxx ${build_dir} && \
+    cd ${build_dir} && \
+    make && \
+    OMP_NUM_THREADS=1 ctest -L unit --output-on-failure && \
+    make install
+fi
+
+exit 0

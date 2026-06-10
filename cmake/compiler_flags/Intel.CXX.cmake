d48039dacd1d415f15bb95ab3f7631345d1e6378
Niklas Goellmann <ngoellmann@gmail.com>
2026-06-10 13:16:36 +0200

    aligned minimum versions of cmake and compilers with default versions of Ubuntu 22.04 LTS

--- cmake/compiler_flags/Intel.CXX.cmake
+++ cmake/compiler_flags/Intel.CXX.cmake
@@ -1,5 +1,6 @@
 if(NOT DEFINED ENV{CXXFLAGS})
-    if(CMAKE_CXX_COMPILER_ID MATCHES Intel)
+    # Intel Classic (icc/icpc) only — IntelLLVM (icx/icpx) uses Clang flags
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftz -fp-speculation=fast -fp-model fast -Wno-unknown-pragmas")
         set(CMAKE_CXX_FLAGS_RELEASE "-O2 -DNDEBUG")
         set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -debug -DNDEBUG")
